d3d031ee375a36945619e887b0889cc206abfeec
Amy Franz <amy.franz@gmail.com>
2020-01-04 12:48:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
-def square_array(array)
-  # your code here
+def square_array(my_array)
+  newArray = []
+  counter = 0
+  while counter < my_array.length do
+    newArray << my_array[counter] **
+    counter ++
+  end
+  return newArray
 end
